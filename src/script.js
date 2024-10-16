8a0775dbf7a0dea93de77d4d3f40716d06691f92
ADRIAN DA SILVA BERLINCK <adrian_berlinck@estudante.sc.senai.br>
2024-10-15 21:16:52 -0300

    prova

--- src/script.js
+++ src/script.js
@@ -1,1 +1,37 @@
-alert("Script conectado a página")
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('formularioVeiculo');
+    const ul = document.getElementById('ulVeiculos');
+
+    const veiculos = JSON.parse(localStorage.getItem('veiculos')) || [];
+    veiculos.forEach(veiculo => adicionarVeiculo(veiculo.nome, veiculo.ano, veiculo.cor, veiculo.imagem));
+
+    function adicionarVeiculo(nome, ano, cor, imagem) {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <img src="${imagem}" alt="${nome}" class="veiculo-imagem" />
+            <div>
+                <h3>Modelo: ${nome}</h3>
+                <h4>Ano: ${ano}</h4>
+                <h4>Cor: ${cor}</h4>
+            </div>
+        `;
+        ul.appendChild(li);
+    }
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const nome = document.getElementById('nome').value;
+        const ano = document.getElementById('ano').value;
+        const cor = document.getElementById('cor').value;
+        const imagem = document.getElementById('imagem').value;
+
+        const veiculo = { nome, ano, cor, imagem };
+        const veiculos = JSON.parse(localStorage.getItem('veiculos')) || [];
+        veiculos.push(veiculo);
+        localStorage.setItem('veiculos', JSON.stringify(veiculos));
+
+        adicionarVeiculo(nome, ano, cor, imagem);
+        this.reset();
+    });
+});
